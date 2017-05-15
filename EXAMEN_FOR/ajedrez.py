f23c37237165c8a5c6a50b701efdf62e32758b50
Carlos Moreno Casquero <carlos.moreno2000@hotmail.com>
2017-05-15 18:54:07 +0200

    Update ajedrez.py

--- EXAMEN_FOR/ajedrez.py
+++ EXAMEN_FOR/ajedrez.py
@@ -1,5 +1,15 @@
+
+####################################
+#  INICAMOS EL BUCLE CON EL FORD   #
+####################################
+
 for fil in range(1,9,1):
 	for col in range(1,9,1):
+		
+
+######################################
+#  PRINTAMOS EL TABLERO DE AJEDREZ   #
+######################################		
 	
 		if (fil== 1 or fil==3 or fil== 5 or fil== 7):
 		
