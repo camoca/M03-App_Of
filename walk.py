bf527d9968cd77befa63464c2cc9726c7b8b85ac
ism25366309 <ism25366309@a17.informatica.escoladeltreball.org>
2017-05-30 11:49:50 +0200

    pruevas_clase

--- walk.py
+++ walk.py
@@ -2,8 +2,11 @@
 # coding: utf-8
 
 import os
+import stat
 
 path_to_explore="./walk/"
+
+total_size= 0
 
 #MOSTREM NOMES ELS DIRECTORIS
 
@@ -48,5 +51,22 @@ for root, dirs, files  in os.walk(path_to_explore):
 	for name in dirs:
 		
 		name_path=os.path.join(root, name)
-		print (name_path)
+		print (name_path),
+		
+#MOATREM EL PES
 	
+		print os.stat(name_path).st_size
+		
+		total_size= total_size + os.stat(name_path).st_size
+		
+#MOSTREM ELS PERMISOS
+		
+		permission= stat.S_IMODE (os.stat(name_path).st_mode)
+		permisos= str(oct(permission))
+		print permisos
+		
+		other= permisos[3:4]
+		if(other=="0"):
+			print "todo bien todo correcto"
+		else:
+			print "CAUTION"
